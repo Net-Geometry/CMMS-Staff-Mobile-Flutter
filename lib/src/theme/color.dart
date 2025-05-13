d4eca190068cd872e9baee808f4ae03d8c082fa5
samsul <ms.arifin29@gmail.com>
2025-05-13 22:20:29 +0800

    enhancement :  improvement theme color and text

--- lib/src/theme/color.dart
+++ lib/src/theme/color.dart
@@ -1,28 +1,78 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
+  /// ARGB 0xFF00A572
   static const primary = Color(0xFF00A572);
+
+  /// ARGB 0xFFD1FAE5
   static const mintGreen = Color(0xFFD1FAE5);
+
+  /// ARGB 0xFFDBEAFE
   static const lightBlue = Color(0xFFDBEAFE);
+
+  /// ARGB 0x1A1DB988
   static const translucentTeal = Color(0x1A1DB988);
+
+  /// ARGB 0xFF2B00FF
   static const electricBlue = Color(0xFF2B00FF);
+
+  /// ARGB 0xFF979797
   static const mediumGray = Color(0xFF979797);
+
+  /// ARGB 0xFFFBBF24
   static const amber = Color(0xFFFBBF24);
+
+  /// ARGB 0xFF000000
   static const black = Color(0xFF000000);
+
+  /// ARGB 0xFFFF00F6
   static const hotPink = Color(0xFFFF00F6);
+
+  /// ARGB 0xFF00FC65
   static const neonGreen = Color(0xFF00FC65);
+
+  /// ARGB 0xFF00EAFF
   static const cyan = Color(0xFF00EAFF);
+
+  /// ARGB 0xFFE5E7EB
   static const lightGray = Color(0xFFE5E7EB);
+
+  /// ARGB 0xFFFEF2F2
   static const softRed = Color(0xFFFEF2F2);
+
+  /// ARGB 0xFF00A6FF
   static const skyBlue = Color(0xFF00A6FF);
+
+  /// ARGB 0xFFFFFBEB
   static const paleYellow = Color(0xFFFFFBEB);
+
+  /// ARGB 0xFFD97706
   static const orange = Color(0xFFD97706);
+
+  /// ARGB 0xFF17A578
   static const jadeGreen = Color(0xFF17A578);
+
+  /// 0xFFFFFFFF
   static const white = Color(0xFFFFFFFF);
+
+  /// ARGB 0xFFDC2626
   static const dangerRed = Color(0xFFDC2626);
+
+  /// ARGB 0xFFEF4444
   static const error = Color(0xFFEF4444);
+
+  /// ARGB 0xFF4B5563
   static const charcoalGray = Color(0xFF4B5563);
+
+  /// ARGB 0xFF6B7280
   static const slateGray = Color(0xFF6B7280);
+
+  /// ARGB 0xFFADAEBC
   static const paleSlate = Color(0xFFADAEBC);
+
+  /// ARGB 0xFFCCC9C9
   static const lightConcrete = Color(0xFFCCC9C9);
+
+  /// ARGB #F9FAFB
+  static const offWhite = Color(0xFFF9FAFB);
 }
--- lib/src/theme/text.dart
+++ lib/src/theme/text.dart
@@ -10,36 +10,73 @@ class AppTextStyle extends TextStyle {
   AppTextStyle({this.size}) : super(fontFamily: _fontFamily, fontSize: size);
 }
 
-/// Make sure you call the font types before overriding the weight and colors
+//// Make sure you call the font types before overriding the weight and colors
 extension AppTextStyleExtension on Text {
+  /// Text style with font size 32
   Text get headlineLarge => Text(data ?? '', style: AppTextStyle(size: 32));
-  Text get headlineMedium => Text(data ?? '', style: AppTextStyle(size: 28));
-  Text get headlineSmall => Text(data ?? '', style: AppTextStyle(size: 24));
 
+  /// Text style with font size 28
+  Text get headlineMedium => Text(data ?? '', style: AppTextStyle(size: 28));
+
+  /// Text style with font size 26
+  Text get headlineSmall => Text(data ?? '', style: AppTextStyle(size: 26));
+
+  /// Text style with font size 24
+  Text get headline => Text(data ?? '', style: AppTextStyle(size: 24));
+
+  /// Text style with font size 22
   Text get titleLarge => Text(data ?? '', style: AppTextStyle(size: 22));
+
+  /// Text style with font size 18
   Text get titleMedium => Text(data ?? '', style: AppTextStyle(size: 18)).titleMedium;
+
+  /// Text style with font size 14
   Text get titleSmall => Text(data ?? '', style: AppTextStyle(size: 14)).titleMedium;
 
+  /// Text style with font size 16
   Text get bodyLarge => Text(data ?? '', style: AppTextStyle(size: 16));
+
+  /// Text style with font size 14
   Text get bodyMedium => Text(data ?? '', style: AppTextStyle(size: 14));
+
+  /// Text style with font size 12
   Text get bodySmall => Text(data ?? '', style: AppTextStyle(size: 12));
 
+  /// Text style with font size 14
   Text get labelLarge => Text(data ?? '', style: AppTextStyle(size: 14)).medium;
+
+  /// Text style with font size 12
   Text get labelMedium => Text(data ?? '', style: AppTextStyle(size: 12)).medium;
+
+  /// Text style with font size 11
   Text get labelSmall => Text(data ?? '', style: AppTextStyle(size: 11)).medium;
 
+  /// Text style with font weight w400
   Text get regular => Text(data ?? '', style: style?.copyWith(fontWeight: FontWeight.w400));
-  Text get medium => Text(data ?? '', style: style?.copyWith(fontWeight: FontWeight.w500));
-  Text get semiBold => Text(data ?? '', style: style?.copyWith(fontWeight: FontWeight.w600));
-  Text get bold => Text(data ?? '', style: style?.copyWith(fontWeight: FontWeight.w700));
 
+  /// Text style with font weight w500
+  Text get medium => Text(data ?? '', style: style?.copyWith(fontWeight: FontWeight.w500));
+
+  /// Text style with font weight w600
+  Text get semiBold => Text(data ?? '', style: style?.copyWith(fontWeight: FontWeight.w600));
+
+  /// Text style with font weight w700
+  Text get bold => Text(data ?? '', style: style?.copyWith(fontWeight: FontWeight.w700));
   Text withColor(Color color) => Text(data ?? '', style: style?.copyWith(color: color));
   Text get white => withColor(Colors.white);
   Text get black => withColor(Colors.black);
   Text get black54 => withColor(Colors.black54);
   Text get black87 => withColor(Colors.black87);
+
+  /// ARGB 0xFFEF4444
   Text get error => withColor(AppColor.error);
+
+  /// ARGB 0xFF00A572
   Text get primary => withColor(AppColor.primary);
+
+  /// ARGB 0xFFADAEBC
   Text get paleSlate => withColor(AppColor.paleSlate);
+
+  /// ARGB 0xFF4B5563
   Text get charcoalGray => withColor(AppColor.charcoalGray);
 }
